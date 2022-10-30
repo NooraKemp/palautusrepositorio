ed002969e41c3fcd47fce9f27ab47fe6d8012d9e
Noora Kemppainen <noora.kemppainen@helsinki.fi>
2022-10-30 11:03:04 +0200

    add tests

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,42 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_palauttaa_none_jos_pelaaja_ei_ole_listalla(self):
+        self.assertEqual(self.statistics.search("Koivu"), None)
+
+    def test_palauttaa_pelaajan(self):
+        self.assertEqual(str(self.statistics.search("Semenko")), "Semenko EDM 4 + 12 = 16")
+
+    def test_palauttaa_listan(self):
+        self.assertEqual(len(self.statistics.team("EDM")), 3)
+
+    def test_palauttaa_topPisteet_oikein(self):
+        self.assertEqual(str(self.statistics.top(3, 1)[0]), "Gretzky EDM 35 + 89 = 124")
+
+    def test_palauttaa_topMaalit_oikein(self):
+        self.assertEqual(str(self.statistics.top(3, 2)[0]), "Lemieux PIT 45 + 54 = 99") 
+
+    def test_palauttaa_topSyoto_oikein(self):
+        self.assertEqual(str(self.statistics.top(3, 3)[0]), "Gretzky EDM 35 + 89 = 124")  
+    
+
+        
+
