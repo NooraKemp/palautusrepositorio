b9c87565b10ff53731a1c958f53b10fd534c18d9
Noora Kemppainen <noora.kemppainen@helsinki.fi>
2022-11-12 10:04:39 +0200

    add tasks 10-12

---
+++ viikko2/verkkokauppa-1/src/kirjanpito.py
@@ -0,0 +1,10 @@
+class Kirjanpito:
+
+    def __init__(self):
+        self.tapahtumat = []
+
+    def lisaa_tapahtuma(self, tapahtuma):
+        self.tapahtumat.append(tapahtuma)
+
+
+kirjanpito = Kirjanpito()
