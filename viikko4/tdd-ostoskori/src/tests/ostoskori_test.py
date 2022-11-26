5c864a4ffdf5f7e1ef302213cb2c2fbc9b1c1025
Noora Kemppainen <noora.kemppainen@helsinki.fi>
2022-11-26 14:36:21 +0200

    kahden tuotteen lisäämisen jälkeen korin hinta on kahden tuotteen hinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -20,9 +20,17 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
 
-    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_tavaraa(self):
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_tavaraa(self):  
         maito = Tuote("Maito", 3)
         piima = Tuote("Piimä", 4)
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(piima)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_on_kahden_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        piima = Tuote("Piimä", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(piima)
+        self.assertEqual(self.kori.hinta(), 7)
